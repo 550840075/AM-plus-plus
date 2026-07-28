0420817b5fca514323c49ce9425f391dc9d32113
Zennmn <176552149+Zennmn@users.noreply.github.com>
2026-07-28 13:38:18 +0800

    release: prepare v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 80
-        versionName = "1.0.79"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
